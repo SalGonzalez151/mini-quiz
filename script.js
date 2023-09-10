a5746d1ae7f5508ef5ccf6b84f3b9c1bc6e5682f
Sal Gonzalez <imdone151@gmail.com>
2023-09-10 14:11:25 -0700

    Added 2 more functions to get question and a new check answer function, also added more variables

--- script.js
+++ script.js
@@ -1,55 +1,85 @@
 var questions = [
-    {
-      title: 'Commonly used data types DO NOT include:',
-      choices: ['strings', 'booleans', 'alerts', 'numbers'],
-      answer: 'alerts',
-    },
-    {
-      title: 'The condition in an if / else statement is enclosed within ____.',
-      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-      answer: 'parentheses',
-    },
-    {
-      title: 'Arrays in JavaScript can be used to store ____.',
-      choices: [
-        'numbers and strings',
-        'other arrays',
-        'booleans',
-        'all of the above',
-      ],
-      answer: 'all of the above',
-    },
-    {
-      title:
-        'String values must be enclosed within ____ when being assigned to variables.',
-      choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-      answer: 'quotes',
-    },
-    {
-      title:
-        'A very useful tool used during development and debugging for printing content to the debugger is:',
-      choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
-      answer: 'console.log',
-    },
-  ];
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts',
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
+  },
+];
+
+var score = 0;
+var questionNumber = 0;
+var startBtn = document.querySelector('.start-button');
+var questionText = document.querySelector('.question-text');
+var answerChoices = document.querySelector('.answers');
+var resultsId = document.querySelector('.results');
+var right = 'Correct!';
+var wrong = 'Wrong!';
+
+function getQuestion() {
+  var getQuestion = questions[questionNumber];
+  questionText.textContent = getQuestion.title;
+
+  answerChoices.textContent = '';
   
-  var score = 0;
-  var questionNumber = 0;
-  var startBtn = document.querySelector('.start-button');
-  var questionText = document.querySelector('.question-text');
-  var answerChoices = document.querySelector('.answers');
-  var resultsId = document.querySelector('.results');
+  var answerList = document.createElement('ol');
 
-  function getQuestion() {
-    var getQuestion = questions[questionNumber];
-    questionText.textContent = getQuestion.title;
-  
-    answerChoices.textContent = '';
-  }
+  getQuestion.choices.forEach(function (choice) {
+    var listItem = document.createElement('li');
+    listItem.textContent = choice;
+    listItem.addEventListener('click', function () {
+      checkAnswer(choice, getQuestion.answer);
+    })
+    answerList.appendChild(listItem);
+  })
+  answerChoices.appendChild(answerList);
 
+}
 
-  function startQuiz() {
-    startBtn.style.display = 'none';
+function checkAnswer(choice, correctChoice) {
+  questionNumber++;
+  if (questionNumber < questions.length) {
     getQuestion();
-
   }
+  if (choice === correctChoice) {
+    resultsId.textContent = right;
+  } else {
+    resultsId.textContent = wrong;
+    timeLeft = timeLeft - 10;
+  }
+}
+
+
+function startQuiz() {
+  startBtn.style.display = 'none';
+  getQuestion();
+
+}
+
+startBtn.addEventListener('click', startQuiz);
